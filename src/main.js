6ea71e88bfa36eb456a81e19807d421ca5c859de
Lucinda Peal <lucindap2002@gmail.com>
2023-09-27 16:51:35 -0400

    added .catch()

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 const promise = welcome();
 
 console.log(promise);
+
+const question = "Will the weather be nice today?";
+const tellPromise = tell();
+tellPromise
+    .then((fortune) => {
+    console.log(question);
+    console.log(fortune);
+  })
+    .catch(console.error);
